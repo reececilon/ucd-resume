61bcee3b6775d89213aa395ea2976427afe30248
reececilon <reecethat@gmail.com>
2022-01-19 21:18:24 +0000

    render user information from github

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -2,8 +2,8 @@ function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 3,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 40.785091,
+            lng: -73.968285
         }
     });
 
@@ -21,6 +21,6 @@ function initMap() {
             label: labels[i % labels.length]
         });
     });
-    
+
     var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
